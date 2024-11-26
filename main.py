ee0a387b143bcb45a48b84b16b73618bfb0e450d
alejandrocl.2522 <alejandrocl.2522@gmail.com>
2024-11-26 20:08:42 +0000

    update app

--- main.py
+++ main.py
@@ -2,3 +2,24 @@ import streamlit as st
 
 st.title("Alejandro")
 st.caption("soy orgullosamente UIS ;P ")
+st.caption("amante las matematicas, casado con el arte")
+
+#titulos
+st.title(grande) 
+st.header(mediano) 
+st.subheader(pequeño)
+
+#textos
+st.markdown("""
+en esto puedes poner cualquier texto
+poner un texto en **negrilla**, *italica* o en ***ambos***
+
+enumeraciones:
+1.primer item
+2.segundo item
+3. tercer item
+
+
+
+
+""")
